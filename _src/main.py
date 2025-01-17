60c5b0a5e371cdf2fec36eb9b3d358285bbe3723
NinjaKinfe1 <ninjakinfe@gmail.com>
2025-01-17 17:54:55 +0300

    starting the project

---
+++ _src/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x600")
+root.resizable(False,False)
+root.iconbitmap("../media/ico/icon.ico")
+root.title("Simple Python World Clock")
+
+
+root.mainloop()
