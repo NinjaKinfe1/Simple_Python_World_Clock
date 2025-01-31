c1a13ba38088846d01311957e8c437c335001138
NinjaKinfe1 <ninjakinfe@gmail.com>
2025-01-31 17:42:08 +0300

    To make the spacebar function on the display

--- _src/main.py
+++ _src/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import requests
+import re
 import time
 
 root = tk.Tk()
@@ -9,27 +10,24 @@ root.iconbitmap("../media/ico/icon.ico")
 root.title("Simple Python World Clock")
 
 #Functions
+def prevent_key(event):
+    pattern=r"[0123456789.\x08-]"
+    if not re.match(pattern, event.char):
+        return "break"
+
 def submit():
     latitude=la_entry.get()
     longitude=lo_entry.get()
     first_page.place_forget()
     second_page.place(width=400, height=600)
-    loop_submit(latitude, longitude)
-
-def loop_submit(latitude, longitude):
-    url=f"https://timeapi.io/api/time/current/coordinate?latitude={latitude}&longitude={longitude}"
-    pull_request=requests.get(url)
-    get_data=pull_request.json()
+    url = f"https://timeapi.io/api/time/current/coordinate?latitude={latitude}&longitude={longitude}"
+    pull_request = requests.get(url)
+    get_data = pull_request.json()
+    print(get_data)
     time_text.config(text=get_data["time"])
-    second_page.after(30000, loop_submit, latitude, longitude)
-
-
-
-
-
-        
-
-
+    date_text_gui.config(text=get_data["date"])
+    day_of_the_week_text_gui.config(text=get_data["dayOfWeek"])
+    time_zone_text_gui.config(text=get_data["timeZone"])
 
 def back():
     first_page.place(width=400, height=600)
@@ -44,27 +42,28 @@ small_title = tk.Label(first_page, text="Clock", font=("Alfa Slab One", 40), bg=
 small_title.place(x=121, y=141)
 
 #Latitude
-la_lable=tk.Label(first_page, text="Latitude", font=("Open Sans Bold",25), bg="#f3f3f3", fg="#333333")
-la_lable.place(x=128,y=237)
-la_subtitle_lable=tk.Label(first_page, text="(Enter a value within the range of -90 to 90.)",font=("Open Sans Regular", 11), bg="#f3f3f3", fg="#333333")
-la_subtitle_lable.place(x=47,y=286)
+la_label=tk.Label(first_page, text="Latitude", font=("Open Sans Bold",25), bg="#f3f3f3", fg="#333333")
+la_label.place(x=128,y=237)
+la_subtitle_label=tk.Label(first_page, text="(Enter a value within the range of -90 to 90.)",font=("Open Sans Regular", 11), bg="#f3f3f3", fg="#333333")
+la_subtitle_label.place(x=47,y=286)
 
 la_entry=tk.Entry(first_page, font=("Open Sans Bold", 15),bd=0, highlightthickness=0)
 la_entry.place(x=40,y=314, width=320, height=50)
+la_entry.bind("<KeyPress>", prevent_key)
 
-#Longitude 
-lo_lable=tk.Label(first_page, text="Longitude", font=("Open Sans Bold",25), bg="#f3f3f3", fg="#333333")
-lo_lable.place(x=116,y=373)
-lo_subtitle_lable=tk.Label(first_page, text="(Enter a value within the range of -180 to 180.)",font=("Open Sans Regular", 11), bg="#f3f3f3", fg="#333333")
-lo_subtitle_lable.place(x=39,y=423)
+#Longitude
+lo_label=tk.Label(first_page, text="Longitude", font=("Open Sans Bold",25), bg="#f3f3f3", fg="#333333")
+lo_label.place(x=116,y=373)
+lo_subtitle_label=tk.Label(first_page, text="(Enter a value within the range of -180 to 180.)",font=("Open Sans Regular", 11), bg="#f3f3f3", fg="#333333")
+lo_subtitle_label.place(x=39,y=423)
 
 lo_entry=tk.Entry(first_page, font=("Open Sans Bold", 15),bd=0, highlightthickness=0)
 lo_entry.place(x=40,y=451, width=320, height=50)
+lo_entry.bind("<KeyPress>", prevent_key)
 
 #Submit Button
 submit=tk.Button(first_page, text="Submit", font=("Open Sans ExtraBold",19), bg="#333333", fg="#ffffff", activebackground="#f3f3f3", activeforeground="#333333", bd=0, highlightthickness=0, command=submit)
 submit.place(x=100,y=522, width=200, height=50)
-
 
 #Second Page
 second_page=tk.Frame(root, bg="#f3f3f3")
